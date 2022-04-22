d338166750d2940761043bf962c0f6e5cf59b1c9
ryandi <purbaryandi@gmail.com>
2022-04-23 02:19:05 +0700

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -382,8 +382,8 @@ const Home = (props: HomeProps) => {
                 Total Minted : {itemsRedeemed} / {itemsAvailable}
               </p>
               <p style={{ color: "white", fontWeight: "bold" }}>
-                {/* Price: {price + " " + priceLabel} */}
-                Price: 0.3 Sol
+                Price: {price + " " + priceLabel}
+                {/* Price: 0.3 Sol */}
               </p>
             </>
           )}
